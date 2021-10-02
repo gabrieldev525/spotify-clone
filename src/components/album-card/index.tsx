ed37ef911a59aead6c0df699859bd697885bdd32
wendelsilva <wendel245.ws@gmail.com>
2021-10-02 16:22:58 -0300

    feat(album-card-component): create album card component

---
+++ src/components/album-card/index.tsx
@@ -0,0 +1,26 @@
+// React
+import React from 'react'
+
+//style
+import "./styles/styles.scss"
+
+export default function AlbumCard() {
+  return (
+    <>
+    <div className="album-card">
+      <div className="album-card-info">
+        <img className="album-card-image" src="https://via.placeholder.com/150" alt="artist album photo" />
+        <p className="album-card-name">Artist Name</p>
+      </div>
+      <div className="play-album">
+        <div className="icon">
+          <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 25 25">
+            <path d="M3 22v-20l18 10-18 10z"/>
+          </svg>
+        </div>
+      </div>
+    </div>
+    
+    </>
+  )
+}
--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,8 +1,10 @@
 // React
 import React from 'react'
 
+import AlbumCard from '../../components/album-card'
+
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <AlbumCard></AlbumCard>
   )
 }
