debd8ad38ddf0c419dd7a7010acc92b4b839f7a1
Aron Matheus Garcia Mendes <mmendes@morphus.com.br>
2021-10-07 00:04:23 -0300

    Ajustes resolvidos referentes aos comentarios

--- src/components/ArtistCard/index.tsx
+++ src/components/ArtistCard/index.tsx
@@ -1,25 +1,25 @@
 // React
 import React from 'react'
 
-// Project
-import ArtistImg from '../../static/images/no-image.png'
-import Playicon from '../../static/icons/play.svg'
+// Third party
+import { IoPlaySharp } from 'react-icons/io5'
 
-// Styles
+// Local
 import './styles/styles.scss'
+import { IArtistCardProps } from './types'
 
-export default function ArtistCard() {
+export default function ArtistCard({ image, title, category }: IArtistCardProps) {
   return (
     <div id='container_main_card_artist'>
-      <div className='container_img'>
-        <img src={ArtistImg} alt='Image do artista' />
+      <div className='container_image_artist'>
+        <img src={image} alt='Image do artista' />
         <div className='container_play_music'>
-          <img src={Playicon} alt='play music' />
+          <IoPlaySharp className='icon_play' size={30} />
         </div>
       </div>
       <div className='container_info_artist'>
-        <h3 className='title_artist'>Queen</h3>
-        <span className='sub_title'>Artista</span>
+        <h3 className='title_artist'>{title}</h3>
+        <span className='sub_title'>{category}</span>
       </div>
     </div>
   )
---
+++ src/components/ArtistCard/types.ts
@@ -0,0 +1,5 @@
+export interface IArtistCardProps {
+  image: string,
+  title: string,
+  category: string
+}
