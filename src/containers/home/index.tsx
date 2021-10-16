abc55b79eb0d55edd922804846e63403b1e8593c
gabrieldev525 <gvictor525.gv@gmail.com>
2021-10-16 19:58:41 -0300

    Item da lista música popular do artista

--- src/containers/home/index.tsx
+++ src/containers/home/index.tsx
@@ -1,8 +1,36 @@
 // React
 import React from 'react'
+import SimpleListItem from '../../components/simple-list-item'
+
+// Project
+import Image from '../../static/images/music.jpeg'
 
 export default function Home() {
   return (
-    <h1>Home</h1>
+    <>
+      <h1>Home</h1>
+
+      <SimpleListItem
+        position={1}
+        musicName='Nome da música'
+        image={Image}
+        totalPlays='2,525,702'
+        timeDuration='2:27'
+        active={true} />
+
+      <SimpleListItem
+        position={1}
+        musicName='Nome da música'
+        image={Image}
+        totalPlays='2,525,702'
+        timeDuration='2:27' />
+
+      <SimpleListItem
+        position={1}
+        musicName='Nome da música'
+        image={Image}
+        totalPlays='2,525,702'
+        timeDuration='2:27' />
+    </>
   )
 }
