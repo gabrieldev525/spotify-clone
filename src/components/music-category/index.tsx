78db4bd7d04bbd2351527e7df787ec37ab3a0e06
wendelsilva <wendel245.ws@gmail.com>
2021-10-23 16:45:31 -0300

    feat(music-category): music category component

--- src/components/music-category/index.tsx
+++ src/components/music-category/index.tsx
@@ -7,7 +7,7 @@ import './styles/styles.scss'
 
 export default function MusicCategory({CategoryTitle, CategoryImage}: IMusicCategory) {
     return (
-      <div className="music-category">
+      <div className="music-category" style={{backgroundColor: `{$color}`}}>
           <div className="music-cateogry-title">{CategoryTitle}</div>
           <img src={CategoryImage} alt="imagem do album da categoria de música" className="music-category-image" />
       </div>
