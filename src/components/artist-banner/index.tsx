2aaf347a32b339659f5653efc3c040f81ed0eb7d
gabrieldev525 <gvictor525.gv@gmail.com>
2021-10-30 20:17:14 -0300

    Página do detalhe do artista

--- src/components/artist-banner/index.tsx
+++ src/components/artist-banner/index.tsx
@@ -5,20 +5,28 @@ import React from 'react'
 import { IArtistBanner } from './types'
 import './styles/styles.scss'
 
-export default function ArtistBanner({artistBannerImage, artistPlaylistName, artistMusicName, artistBannerName}: IArtistBanner) {
-    return (
-      <div className='artist-banner'>
-          <img src={artistBannerImage} alt='artist banner image' />
-          <div className='artist-banner-content'>
-              <p>PlayList</p>
-              <p className='artist-playlist-name'>{artistPlaylistName}</p>
-              <p>Ouça agora '{artistMusicName}' a nova faixa {artistBannerName}</p>
-              <div className='artist-banner-actions'>
-                  <button className='artist-banner-button play'>PLAY</button>
-                  <button className='artist-banner-button follow'>SEGUIR</button>
-              </div>
-          </div>
+export default function ArtistBanner({
+  artistBannerImage,
+  artistPlaylistName,
+  artistMusicName,
+  artistBannerName
+}: IArtistBanner) {
+  return (
+    <div className="artist-banner">
+      {artistBannerImage && (
+        <img src={artistBannerImage} alt="artist banner image" />
+      )}
+      <div className="artist-banner-content">
+        <p>PlayList</p>
+        <p className="artist-playlist-name">{artistPlaylistName}</p>
+        <p>
+          Ouça agora '{artistMusicName}' a nova faixa {artistBannerName}
+        </p>
+        <div className="artist-banner-actions">
+          <button className="artist-banner-button play">PLAY</button>
+          <button className="artist-banner-button follow">SEGUIR</button>
+        </div>
       </div>
-    )
-  }
-
+    </div>
+  )
+}
--- src/components/artist-banner/types.ts
+++ src/components/artist-banner/types.ts
@@ -1,5 +1,5 @@
 export interface IArtistBanner {
-    artistBannerImage: string,
+    artistBannerImage?: string,
     artistPlaylistName: string,
     artistMusicName: string,
     artistBannerName: string
